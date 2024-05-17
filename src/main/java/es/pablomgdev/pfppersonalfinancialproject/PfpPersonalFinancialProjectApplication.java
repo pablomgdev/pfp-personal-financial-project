3ddc86b2d8e4b8bc0c5d70950cae5116138850d0
pablomgdev <111525176+pablomgdev@users.noreply.github.com>
2024-05-17 19:46:43 +0200

    Set up project

---
+++ src/main/java/es/pablomgdev/pfppersonalfinancialproject/PfpPersonalFinancialProjectApplication.java
@@ -0,0 +1,13 @@
+package es.pablomgdev.pfppersonalfinancialproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PfpPersonalFinancialProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PfpPersonalFinancialProjectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/es/pablomgdev/pfppersonalfinancialproject/PfpPersonalFinancialProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package es.pablomgdev.pfppersonalfinancialproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PfpPersonalFinancialProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
